0a8990b06eafe12e1719ebd6a74d1e1446567219
Nataly Rybak <natalya.rybak@gmail.com>
2018-06-17 23:16:37 +0300

    Lab2

--- Assets/Scripts/HeroRabbit.cs
+++ Assets/Scripts/HeroRabbit.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class HeroRabbit : MonoBehaviour {
 	public float speed = 1;
 	public Rigidbody2D myBody = null;
+	bool isGrounded = false;
+	bool JumpActive = false;
+	float JumpTime = 0f;
+	public float MaxJumpTime = 2f;
+	public float JumpSpeed = 2f;
 
 	// Use this for initialization
 	void Start () {
 		myBody = this.GetComponent<Rigidbody2D> ();
-
+		LevelController.current.setStartPosition (transform.position);
 	}
 
 	// Update is called once per frame
@@ -27,6 +32,52 @@ public class HeroRabbit : MonoBehaviour {
 		} else if(value > 0) {
 			sr.flipX = false;
 		}
+
+		Animator animator = GetComponent<Animator> ();
+		if(Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
+		} else {
+			animator.SetBool ("run", false);
+		}
+
+		if(this.isGrounded) {
+			animator.SetBool ("jump", false);
+		} else {
+			animator.SetBool ("jump", true);
+		}
+
+		Vector3 from = transform.position + Vector3.up * 0.3f;
+		Vector3 to = transform.position + Vector3.down * 0.1f;
+		int layer_id = 1 << LayerMask.NameToLayer ("Ground");
+//Перевіряємо чи проходить лінія через Collider з шаром Ground
+		RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+		if(hit) {
+			isGrounded = true;
+		} else {
+			isGrounded = false;
+	}
+//Намалювати лінію (для розробника)
+Debug.DrawLine (from, to, Color.red);
+     if(Input.GetButtonDown("Jump") && isGrounded) {
+		this.JumpActive = true;
+	}
+	if(this.JumpActive) {
+//Якщо кнопку ще тримають
+	if(Input.GetButton("Jump")) {
+		this.JumpTime += Time.deltaTime;
+			if (this.JumpTime < this.MaxJumpTime) {
+				Vector2 vel = myBody.velocity;
+				vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+				myBody.velocity = vel;
+			}
+	} else {
+	this.JumpActive = false;
+	this.JumpTime = 0;
+	}
+}
+
+
+
 	}
 	// Update is called once per frame
 	void Update () {
---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour {
+
+	public static LevelController current;
+	Vector3 startingPosition;
+
+	void Awake() {
+		current = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setStartPosition(Vector3 pos) {
+		this.startingPosition = pos;
+	}
+	public void onRabbitDeath(HeroRabbit rabit) {
+		//При смерті кролика повертаємо на початкову позицію
+		rabit.transform.position = this.startingPosition;
+	}
+}
